4fdde73d8a6b452de370c25286fd2401893a423c
Pedro Costa <pedrocostaalves@live.com.pt>
2023-11-27 12:43:49 +0000

    SQL Script update

--- sql/create_db_tables.sql
+++ sql/create_db_tables.sql
@@ -15,7 +15,7 @@ CREATE TABLE tasks (
     title VARCHAR(100) NOT NULL,
     description TEXT,
     due_date DATE,
-    status VARCHAR(20) DEFAULT 'TO-DO',
+    status VARCHAR(20) DEFAULT 'TO_DO',
     user_id INT,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
@@ -28,8 +28,8 @@ INSERT INTO users (username, email) VALUES
 
 -- Insert Tasks
 INSERT INTO tasks (title, description, due_date, status, user_id) VALUES
-('Complete Project Proposal', 'Write a detailed proposal for the upcoming project.', '2023-12-01', 'TO-DO', 1),
-('Design Database Schema', 'Create a database schema for the task management system.', '2023-12-05', 'IN PROGRESS', 1),
-('Implement User Authentication', 'Integrate user authentication for the Spring Boot API.', '2023-12-10', 'TO-DO', 2),
-('Review Codebase', 'Perform a code review for the latest changes in the codebase.', '2023-12-08', 'TO-DO', 3),
-('Deploy API to Production', 'Prepare the API for production deployment and launch.', '2023-12-15', 'TO-DO', 3);
+('Complete Project Proposal', 'Write a detailed proposal for the upcoming project.', '2023-12-01', 'TO_DO', 1),
+('Design Database Schema', 'Create a database schema for the task management system.', '2023-12-05', 'IN_PROGRESS', 1),
+('Implement User Authentication', 'Integrate user authentication for the Spring Boot API.', '2023-12-10', 'TO_DO', 2),
+('Review Codebase', 'Perform a code review for the latest changes in the codebase.', '2023-12-08', 'TO_DO', 3),
+('Deploy API to Production', 'Prepare the API for production deployment and launch.', '2023-12-15', 'TO_DO', 3);
